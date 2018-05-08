f713849ec076a6d4562ee3ddfbab0a2d02e81dc0
Wayne Du <waynestrive@outlook.com>
2018-05-08 18:37:56 -0400

    123456

--- python_scripts/mymodules/vsearch.py
+++ python_scripts/mymodules/vsearch.py
@@ -2,12 +2,12 @@
 
 def search4vowels(word:str)->set:
 
-    # Returns a boolean based on any vowels found
+    """ Returns a boolean based on any vowels found """
     
     vowels = set('aeiou')
     found = vowels.intersection(set(word))
     return bool(found)
 
 def search4letters(phrase:str, letters:str = 'aeiou')->set:
-    # Return a set of the 'letters' found in 'phrase'.
+    """ Return a set of the 'letters' found in 'phrase'. """
     return set(letters).intersection(set(phrase))
