945ac1662f65881b091dbc95ba81a9572fad6148
wd <ddwwzzyy@gmail.com>
2018-05-08 19:10:15 -0400

    I don't known what's happened

--- python_scripts/vsearch.py
+++ python_scripts/vsearch.py
@@ -1,13 +1,12 @@
 
+def search4vowels(word:str) ->set:
 
-def search4vowels(word:str)->set:
-
-    # Returns a boolean based on any vowels found
+    """ Returns a boolean based on any vowels found """
     
     vowels = set('aeiou')
     found = vowels.intersection(set(word))
-    return bool(found)
+    return found
 
-def search4letters(phrase:str, letters:str = 'aeiou')->set:
-    # Return a set of the 'letters' found in 'phrase'.
+def search4letters(phrase:str, letters:str = 'aeiou') ->set:
+    """ Return a set of the 'letters' found in 'phrase'. """
     return set(letters).intersection(set(phrase))
