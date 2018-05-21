0caaa688055bcaa3f2be442ef317ccbcfa0c9c08
wd <ddwwzzyy@gmail.com>
2018-05-21 17:09:27 -0400

    Use unordered_map with customized class by boost library

---
+++ cpp_scripts/accept.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+bool accept3(){
+    int tries = 1;
+    while(tries < 4){
+        cout<< "Do you want to proceed(y or n)?\n";
+        char answer = 0;
+        cin>> answer;
+
+        switch(answer){
+            case 'y':
+                return true;
+            case 'n':
+                return false;
+            default:
+                cout<< "Sorry, I don't understand that.\n";
+                ++tries;
+        }
+    }
+    cout<< "I'll take that for a no.\n";
+    return false;
+}
