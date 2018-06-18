098f2b86d1c5c4225adec4d696c9abb169e35d48
wd <ddwwzzyy@gmail.com>
2018-06-18 16:28:46 -0400

    program of loading stl file and converting it into voxels succeded

--- cpp_scripts/stl_loading/stl_load.cpp
+++ cpp_scripts/stl_loading/stl_load.cpp
@@ -11,6 +11,8 @@
  */
 
 #include <vector>
+#include <iostream>
+#include <fstream>
 #include "stl_reader.h"
 #include <eigen3/Eigen/Dense>
 #include <smpl/geometry/voxelize.h>
@@ -36,55 +38,48 @@ int main(){
 	std::vector<std::uint32_t> tris, solids;
 
 	try {
-		stl_reader::ReadStlFile ("mount.stl", coords, normals, tris, solids);
+        const char* sEnvFile = "mount.stl";
+
+        // triangles are loaded successfully
+		stl_reader::ReadStlFile (sEnvFile, coords, normals, tris, solids);
 
 
 		const size_t numTris = tris.size() / 3;
-        // loop through triangles
-        // itri: indices of the current triangle
+
 		for(size_t itri = 0; itri < numTris; ++itri) {
 
-			//std::cout << "coordinates of triangle " << itri << ": ";
-            //std::cout<< "tirangle index: " << itri;
-            indices.push_back((std::uint32_t)itri);
+            indices.push_back((std::uint32_t)itri*3 + 0);
+            indices.push_back((std::uint32_t)itri*3 + 1);
+            indices.push_back((std::uint32_t)itri*3 + 2);
 
-            //icorner: the ith corner of current triangle
             for(size_t icorner = 0; icorner < 3; ++icorner) {
                 float* c = &coords[3 * tris [3 * itri + icorner]];
-                //std::cout<<" push_back vertices   ";
-                //std::cout << "(" << c[0] << ", " << c[1] << ", " << c[2] << ") "<< std::endl;
+                
                 Eigen::Vector3d currentVertex((double)c[0],(double)c[1], (double)c[2]);
                 vertices.push_back(currentVertex);
-		  	}
+            }
 
-            // norm of current triangle
-/*
- *            std::cout << std::endl;
- *            float* n = &normals [3 * itri];
- *            std::cout	<< "normal of triangle " << itri << ": "
- *                << "(" << n[0] << ", " << n[1] << ", " << n[2] << ")\n";
- *
- */
-		}   // end for 
+        }   // end for 
 
-
-	}
-	catch (std::exception& e) {
+	} catch (std::exception& e) {
 		std::cout << e.what() << std::endl;
 	}
 
     bool fill = false;
 
-    std::cout<< "index size: "<< indices.size() %3<<std::endl;
-
+    std::cout<< "number of triangles: "<< indices.size()/3<<std::endl;
     VoxelizeMesh(vertices, indices, res, voxels, fill);
-    std::cout<< "number of triangles: "<< indices.size()<<std::endl;
-    std::cout<< "number of triangles: "<< voxels.size()<<std::endl;
-
-    // convert mesh data into voxels
 
     std::cout<< "finish voxelizing mesh." << std::endl;
     
+
+
+    // write those voxels into a txt file and check
+    std::ofstream vxls;
+    vxls.open("voxels.txt");
+    for(auto voxel:voxels){
+        vxls << voxel.x() << " " << voxel.y() << " " << voxel.z() <<std::endl;             
+    }
  
     return 0;
 }
