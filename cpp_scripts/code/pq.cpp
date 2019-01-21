12d2e8395290b829651551acba642bc03e801afd
Wayne Du <ddwwzzyy@gamil.com>
2019-01-21 00:05:21 -0500

    homeowrk template

--- cpp_scripts/code/pq.cpp
+++ cpp_scripts/code/pq.cpp
@@ -1,64 +1,59 @@
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include <queue>
-#include <ctime>
 #include <vector>
-#include <cstdlib>
-#include <algorithm>
 
-struct node{
+struct node {
     int a;
     int f;
 
-    node(int x, int y):a(x), f(y){}
+    node(int x, int y) : a(x), f(y) {}
 };
 
-
-struct compare{
-    bool operator()(const node* state1, const node* state2){
+struct compare {
+    bool operator()(const node* state1, const node* state2) {
         return state1->f > state2->f;
     }
 };
 
-class custom_priority_queue : public std::priority_queue<node*, std::vector<node*>, compare>
-{
-  public:
-
-      bool remove(const node* &value) {
+class custom_priority_queue
+    : public std::priority_queue<node*, std::vector<node*>, compare> {
+public:
+    bool remove(const node*& value) {
         auto it = std::find(this->c.begin(), this->c.end(), value);
         if (it != this->c.end()) {
             this->c.erase(it);
             std::make_heap(this->c.begin(), this->c.end(), this->comp);
             return true;
-       }
-       else {
-        return false;
-       }
- }
+        } else {
+            return false;
+        }
+    }
 };
 
-
-
-int main(){
+int main() {
     std::srand(std::time(NULL));
-    std::priority_queue<node*, std::vector<node*>,compare> open;
+    std::priority_queue<node*, std::vector<node*>, compare> open;
 
-    node* p = new node(-1,-1);
+    node* p = new node(-1, -1);
     open.push(p);
 
-    for(int i = 0; i< 50; ++i){
-        node* m = new node(std::rand()%200, std::rand()%200);
+    for (int i = 0; i < 50; ++i) {
+        node* m = new node(std::rand() % 200, std::rand() % 200);
         open.push(m);
     }
 
-//    p->f = 300;
-    node* resort = new node(-5,-5);
+    //    p->f = 300;
+    node* resort = new node(-5, -5);
     open.push(resort);
     open.pop();
-    for(; !open.empty(); ){
-        std::cout<<(open.top()->f)<<"  "<<(open.top()->a)<<std::endl;
+    for (; !open.empty();) {
+        std::cout << (open.top()->f) << "  " << (open.top()->a) << std::endl;
         open.pop();
     }
-  
+
     return 0;
 }
 
