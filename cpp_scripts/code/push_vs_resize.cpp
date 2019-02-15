4cc2050e6c26603d00a265f0ee8c94b8dfd1ca6e
Wayne Du <ddwwzzyy@gamil.com>
2019-02-15 13:20:07 -0500

    push_back is faster than resize and assignment

---
+++ cpp_scripts/code/push_vs_resize.cpp
@@ -0,0 +1,32 @@
+/*
+ * To calculate if  resize and assginment is faster 
+ * than the push back function.
+ *
+ * The push_back operation is obviously faster than the
+ * resize and assignment opeartion.
+ *
+ */
+
+#include <vector>
+#include <ctime>
+#include <iostream>
+
+int main () {
+    std::vector<uint64_t> push;
+    std::vector<uint64_t> assign;
+    
+    std::clock_t start = std::clock();
+    for(uint64_t i = 0; i < 1e8; ++i) push.push_back(i); 
+    std::clock_t end = std::clock();
+    auto t1 = (end-start) / CLOCKS_PER_SEC;
+
+    start = std::clock();
+    for(uint64_t i = 0; i < 1e8; ++i) {
+        assign.resize(i+1);
+        assign.at(i) = i;
+    }
+    end  = std::clock();
+    auto t2 = (end-start) / CLOCKS_PER_SEC; 
+
+    std::cout << "t1: " << t1 << " t2  " << t2 << std::endl;
+}
