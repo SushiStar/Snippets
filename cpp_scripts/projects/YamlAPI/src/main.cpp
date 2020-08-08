b0339581c31b32c06bfdba6002c63d2e119a4ea4
wweedd <ddwwzzyy@gmail.com>
2020-08-07 23:52:23 -0700

    read objects

--- cpp_scripts/projects/YamlAPI/src/main.cpp
+++ cpp_scripts/projects/YamlAPI/src/main.cpp
@@ -7,6 +7,7 @@
 // STL include
 #include <iostream>
 #include <string>
+#include <vector>
 
 // other dependencies
 #include <yaml-cpp/yaml.h>
@@ -15,9 +16,21 @@ int main(int argc, char** argv) {
 
   YAML::Node config = YAML::LoadFile(argv[1]);
 
-  std::string name = config["name"].as<std::string>();
-  double age = config["age"].as<double>();
+  int freq = config["Frequency"].as<int>();
+  std::cout << "Frequency: " << freq << std::endl;
 
-  std::cout << "Name: "<< name << ", age: " << age << std::endl;
+  const YAML::Node &obslist = config["Obstacle List"];
+
+  for (YAML::const_iterator it = obslist.begin(); it != obslist.end(); ++it) {
+    const YAML::Node& obj(*it);
+    std::cout << "Type: " << obj["type"].as<std::string>() << std::endl;
+
+    std::vector<double> pose(obj["pose"].as<std::vector<double>>());
+    std::cout << "Pose: " << pose[0] << " " << pose[1] << " " << pose[2] << std::endl;
+
+    std::cout << "Velocity: " << obj["velocity"].as<double>() << std::endl;
+    std::cout << "Acceleration: " << obj["acceleration"].as<double>() << std::endl << std::endl;
+  }
+
   return 0;
 }
