65d554af02807ebcdcd5190ccb8f09a2b8480162
sushicoder <ddwwzzyy@gmail.com>
2021-11-07 14:19:19 -0800

    feat: java

---
+++ java/MenuComponent.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * 
+ * 
+ */
+
+public abstract class MenuComponent {
+    public void add(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void remove(MenuComponent menuComponent) {
+
+    }
+
+    
+} 
