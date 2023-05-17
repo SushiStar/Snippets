f3033f30bef77ea342b30ce88cb3aa7c7b4be083
Wayne Du <ddwwzzyy@gmail.com>
2023-05-16 20:26:39 -0700

    Go: array

---
+++ go/array.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+    // array
+	// var ages [3]int = [3]int{20, 25, 30}
+    var ages = [3]int{20, 25, 30}
+
+    names := [4]string{"Yoshi", "Mario", "Peach", "Bowser"}
+    names[1] = "Luigi"
+
+    fmt.Println(ages, len(ages))
+    fmt.Println(names, len(names))
+
+    // slices (use arrays under the hood)
+    // but we can manipulate them more easily
+    var scores = []float32{100, 50, 60}
+    scores[2] = 25
+    scores = append(scores, 85)
+    fmt.Println(scores, len(scores))
+
+    // slice ranges
+    rangeOne := names[1:4]
+    fmt.Println(rangeOne)
+
+    rangeTwo := names[2:]
+    fmt.Println(rangeTwo)
+
+    rangeOne = append(rangeOne, "koopa")
+    fmt.Println(rangeOne)
+}
--- go/print.go
+++ go/print.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func main1() {
     // print no new line added
     fmt.Print("Hello, ")
     fmt.Print("World!\n")
