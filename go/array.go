f56fcaf337dfbb5c1ea232635472fa9e89f84446
Wayne Du <ddwwzzyy@gmail.com>
2023-05-16 22:22:22 -0700

    Go: import

--- go/array.go
+++ go/array.go
@@ -3,30 +3,30 @@ package main
 import "fmt"
 
 func main1() {
-    // array
+	// array
 	// var ages [3]int = [3]int{20, 25, 30}
-    var ages = [3]int{20, 25, 30}
+	var ages = [3]int{20, 25, 30}
 
-    names := [4]string{"Yoshi", "Mario", "Peach", "Bowser"}
-    names[1] = "Luigi"
+	names := [4]string{"Yoshi", "Mario", "Peach", "Bowser"}
+	names[1] = "Luigi"
 
-    fmt.Println(ages, len(ages))
-    fmt.Println(names, len(names))
+	fmt.Println(ages, len(ages))
+	fmt.Println(names, len(names))
 
-    // slices (use arrays under the hood)
-    // but we can manipulate them more easily
-    var scores = []float32{100, 50, 60}
-    scores[2] = 25
-    scores = append(scores, 85)
-    fmt.Println(scores, len(scores))
+	// slices (use arrays under the hood)
+	// but we can manipulate them more easily
+	var scores = []float32{100, 50, 60}
+	scores[2] = 25
+	scores = append(scores, 85)
+	fmt.Println(scores, len(scores))
 
-    // slice ranges
-    rangeOne := names[1:4]
-    fmt.Println(rangeOne)
+	// slice ranges
+	rangeOne := names[1:4]
+	fmt.Println(rangeOne)
 
-    rangeTwo := names[2:]
-    fmt.Println(rangeTwo)
+	rangeTwo := names[2:]
+	fmt.Println(rangeTwo)
 
-    rangeOne = append(rangeOne, "koopa")
-    fmt.Println(rangeOne)
+	rangeOne = append(rangeOne, "koopa")
+	fmt.Println(rangeOne)
 }
